d983a7d21948b88d9f450cff50b02efb24ba554e
HFproject <92003043+HFproject@users.noreply.github.com>
2023-10-27 14:30:07 -0500

    Add files via upload

--- Reduccion/Reduccion_total.sh
+++ Reduccion/Reduccion_total.sh
@@ -3,7 +3,7 @@
 echo "Iniciando Filtrado y Reduccion de datos HF ..."
 
 echo "Iniciando F0"
-screen -S "Reduccion_F0" -d -m ./Filtrado.sh "/media/soporte/PROCDATA/" 0 f0 "2023/02/01" "/media/soporte/PROCDATA/Reducted/JROB/" 12
+screen -S "Reduccion_F0" -d -m ./Filtrado.sh "/media/soporte/Data/" 0 f0 $1 "/media/soporte/PROCDATA/Reducted/JROA/" 11
 
 for i in {1..4}
 do
@@ -12,5 +12,5 @@ do
 done
 
 echo "Iniciando F1"
-screen -S "Reduccion_F1" -d -m ./Filtrado.sh "/media/soporte/PROCDATA/" 0 f1 "2023/02/01" "/media/soporte/PROCDATA/Reducted/JROB/" 12
+screen -S "Reduccion_F1" -d -m ./Filtrado.sh "/media/soporte/Data/" 0 f1 $1 "/media/soporte/PROCDATA/Reducted/JROA/" 11
 
